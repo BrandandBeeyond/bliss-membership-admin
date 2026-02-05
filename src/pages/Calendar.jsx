61d9c6a7c988b0aa073271bebb3ff4748a784029
dhiraj <dhirajmymodak22@gmail.com>
2026-02-05 22:09:08 +0530

    some changes

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,7 +3,6 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { EventInput, DateSelectArg, EventClickArg } from "@fullcalendar/core";
 import { Modal } from "../components/ui/modal";
 import { useModal } from "../hooks/useModal";
 import PageMeta from "../components/common/PageMeta";
